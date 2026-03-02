b3033c9fd24d4b75d1c07695ee1bf72d09db5c74
Bernard Aurellano <allenbernardrocamora.aurellano25@bicol-u.edu.ph>
2026-03-02 21:08:13 +0800

    Organized notes

--- comments.c
+++ comments.c
@@ -3,15 +3,23 @@
 //3rd push
 //Check to see if git push origin master --set-upstream worked
 
-/*
-git add .
-git commit (will not be pushed if not committed even if already added)
-git push 
-*/
 
 //Check how git ammend function
 
 /*
 git commit --amend -m "message" (amends commit)
 git push -f (overwrites previous push)
+*/
+
+/*
+    Sync changes from computer to GitHub
+        1. git add .
+        2. git commit (will not be pushed if not committed even if already added)
+        3. git push 
+*/
+
+/*
+    Sync changes from GitHub to Computer
+        1. git clone url foldername
+        2.
 */
