0ad88efdd2e593f9443c50cb26dc821c31e2c8e0
Bernard Aurellano <allenbernardrocamora.aurellano25@bicol-u.edu.ph>
2026-03-02 22:09:06 +0800

    Adding application scenario of git and github

--- comments.c
+++ comments.c
@@ -23,3 +23,21 @@ git push -f (overwrites previous push)
         1. git clone url folder_name (creates a local copy from the remote)
         2.
 */
+
+/*
+    Application Scenario of Git and GitHub:
+        1. Has an existing project and want to integrate it into GitHub
+            a. Add git using git init to create local repository.
+            b. git add then git commit
+            c. git remote  add origin url (syncs the local to remote repository)
+            d. Push finished codes to remote repository.
+
+        2. Starting a new project
+            a. create github repository
+            b. clone it
+            c. work on it, commit, then push
+
+        3. Just joined a team who already has remote reposity
+            a. clone repository
+            b. contribute, git add, git commit, git push
+*/
