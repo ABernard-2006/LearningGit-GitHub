fe788ee4c601b498b7fb7d5c8f3341dd56fb160a
Bernard Aurellano <allenbernardrocamora.aurellano25@bicol-u.edu.ph>
2026-03-02 20:32:02 +0800

    Comment Process

--- comments.c
+++ comments.c
@@ -2,3 +2,9 @@
 //Testing git push second time
 //3rd push
 //Check to see if git push origin master --set-upstream worked
+
+/*
+git add .
+git commit (will not be pushed if not committed even if already added)
+git push 
+*/
